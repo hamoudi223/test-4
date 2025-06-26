d0ea854eab810d2b85c8456cbb84d641f20e32f9
Dream <166921709+hamoudi223@users.noreply.github.com>
2025-06-26 22:29:17 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,20 @@
-const admin = require("firebase-admin");
-const { initializeApp, applicationDefault } = require("firebase-admin/app");
-const { getDatabase } = require("firebase-admin/database");
-const serviceAccount = require("./firebase-config.json");
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const { getStorage } = require('firebase-admin/storage');
+require('dotenv').config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
+const firebaseConfig = {
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    // attention à bien formater ta clé privée avec \n
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  storageBucket: process.env.FIREBASE_BUCKET,
+};
 
-module.exports = getDatabase();
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const bucket = getStorage(firebaseApp).bucket();
+
+module.exports = { firebaseApp, db, bucket };
